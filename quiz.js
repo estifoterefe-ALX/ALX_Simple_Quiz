f8d1fe7adb8cf7fde673a84c94be7c53a6be6792
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2025-11-30 11:02:32 +0300

    fixes

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer() {
   const correctAnswer = 4;
   const userAnswer = document.querySelector('[name="quiz"]:checked').value;
   const feedback = document.getElementById("feedback");
-  if (Number(userAnswer) === correctAnswer) {
+  if (userAnswer === correctAnswer) {
     feedback.textContent = "Correct! Well done.";
   } else {
     feedback.textContent = "That's incorrect. Try again!";
